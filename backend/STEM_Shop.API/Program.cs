b02483c0c65840fccb439e996c049d68737c0699
Dương Trung Quốc <quocbamt@gmail.com>
2026-03-23 08:58:06 +0700

    cart, cartItem

--- backend/STEM_Shop.API/Program.cs
+++ backend/STEM_Shop.API/Program.cs
@@ -92,6 +92,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IBrandService, BrandService>();
+builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddTransient<IEmailService, EmailService>();
 
 var app = builder.Build();
--- backend/STEM_Shop.Data/Models/User.cs
+++ backend/STEM_Shop.Data/Models/User.cs
@@ -25,5 +25,7 @@ public partial class User
 
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
     public virtual Role Role { get; set; }
 }
