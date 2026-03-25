050105b976db45925019c75b46839940330671eb
Loiprofpt <loipvse181843@fpt.edu.vn>
2026-03-25 22:44:17 +0700

    fix

--- frontend/src/api/orderApi.js
+++ frontend/src/api/orderApi.js
@@ -1,13 +1,10 @@
 import api from './axiosConfig';
 
 export const orderApi = {
-  create: (orderData) => api.post('/orders/from-cart', orderData),
+  create: (orderData) => api.post('/orders', orderData),
   getMyOrders: () => api.get('/orders/my'),
   getById: (id) => api.get(`/orders/${id}`),
   cancelMyOrder: (id) => api.post(`/orders/my/${id}/cancel`),
-<<<<<<< Updated upstream
-=======
   getAll: () => api.get('/orders'),
   updateStatus: (id, statusData) => api.put(`/orders/${id}`, statusData),
->>>>>>> Stashed changes
 };
